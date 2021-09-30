77525c6311362b00403c0bda10cbe1b02aa1f4b5
tatiastep <tetiana.step.st@gmail.com>
2021-09-30 16:29:34 +0300

    homework changes added

--- hw-02-loops/script.js
+++ hw-02-loops/script.js
@@ -1,26 +1,26 @@
-let n = '';
-let m = '';
+let n;
+let m;
 
-do{
-  n = +prompt('Now you can calculate the sum of numbers between N and M.\n Enter first number N.');
-} while (isNaN(n) || !Number.isInteger(n) || n === 0);
+do {
+  n = prompt('Now you can calculate the sum of numbers between N and M.\n Enter first number N.').trim();
+} while (isNaN(n) || !Number.isInteger(+n) || n.length === 0);
 
-do{
-  m= +prompt('Enter second number M.');
-} while (isNaN(m) || !Number.isInteger(m) || m === 0);
+do {
+  m = prompt('Enter second number M.').trim();
+} while (isNaN(m) || !Number.isInteger(+m) || n.length === 0);
 
-let minNumber = Math.min(n, m);
-let maxNumber = Math.max(n, m);
+const minNumber = Math.min(n, m);
+const maxNumber = Math.max(n, m);
 
 const skipEvenNumbers = confirm('Do you want to sum without even numbers?');
-  
+
 let sum = 0;
 
 for (let i = minNumber; i <= maxNumber; i++) {
-   if (skipEvenNumbers && i % 2 == 0) {
-      continue;
-   } 
-   sum += i;
+  if (skipEvenNumbers && i % 2 == 0) {
+    continue;
+  }
+  sum += i;
 }
 
 alert(`Sum from ${minNumber} to ${maxNumber} is ${sum}`);
