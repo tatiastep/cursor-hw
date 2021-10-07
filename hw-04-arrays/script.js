9afbe2f088c5faf05b23903c04fcb662302515d1
tatiastep <tetiana.step.st@gmail.com>
2021-10-07 16:47:05 +0300

    corrected syntax

--- hw-04-arrays/script.js
+++ hw-04-arrays/script.js
@@ -8,14 +8,14 @@ const getPairs = (students) => {
    const female = students.filter(student => student[student.length - 1] === 'а');
    const pairs = male.map((student, i) => [male[i], female[i]]);
    return pairs;
-}
+};
 
 // №2 Зіставте пари з попереднього завдання та теми проєктів, над якими студенти будуть працювати. 
 const joinPairsWithThemes = (pairs, themes) => {
    const joinedPairs = pairs.map(student => student.join(' i '));
    const pairsWithThemes = joinedPairs.map((pair, i) => [pair, themes[i]]);
    return pairsWithThemes;
-}
+};
 
 // №3 Зіставте оцінки (marks) зі студентом (students).
 const addMarks = (students, marks) => students.map((student, i) => [student, marks[i]]);
