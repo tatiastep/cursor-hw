520dc6ff1c9b9bdf07e876a06ac7d5976b1cf213
tatiastep <tetiana.step.st@gmail.com>
2021-11-07 16:19:17 +0200

    script added

---
+++ hw-10-events/script.js
@@ -0,0 +1,34 @@
+function playAudioWithKeydown(e) {
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  playAudio(audio);
+  key.classList.add('playing');
+};
+
+function playAudioWithMouseClick(e) {
+  const key = e.target.closest('.key');
+  const audio = document.querySelector(`audio[data-key="${key.dataset.key}"]`);
+  playAudio(audio);
+  key.classList.add('playing');
+};
+
+function playAudio(audio) {
+  if (!audio) return;
+  audio.currentTime = 0;
+  audio.play();
+};
+
+function removeTransition(e) {
+  if (e.propertyName === 'transform') {
+    console.log(this);
+    this.classList.remove('playing');
+  }
+};
+
+document.addEventListener('click', playAudioWithMouseClick);
+document.addEventListener('keydown', playAudioWithKeydown);
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+
