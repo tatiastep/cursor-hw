f51f4411f7440325973041ae55c90e6481d99e5f
tatiastep <tetiana.step.st@gmail.com>
2021-11-08 23:20:26 +0200

    homework done

--- hw-10-events/script.js
+++ hw-10-events/script.js
@@ -1,12 +1,13 @@
-function removeTransition(e) {
-  if (e.propertyName === 'transform') {
-    this.classList.remove('playing');
-  }
+function playAudio(audio) {
+  if (!audio) return;
+  audio.currentTime = 0;
+  audio.play();
 };
 
 function playAudioWithKeydown(e) {
   const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+
   playAudio(audio);
   key.classList.add('playing');
 };
@@ -14,17 +15,16 @@ function playAudioWithKeydown(e) {
 function playAudioWithMouseClick(e) {
   const key = e.target.closest('.key');
   const audio = document.querySelector(`audio[data-key="${key.dataset.key}"]`);
+
   playAudio(audio);
   key.classList.add('playing');
 };
 
-function playAudio(audio) {
-  if (!audio) return;
-  audio.currentTime = 0;
-  audio.play();
+function removeTransition(e) {
+  if (e.propertyName === 'transform') {
+    this.classList.remove('playing');
+  }
 };
-
-
 
 document.addEventListener('click', playAudioWithMouseClick);
 document.addEventListener('keydown', playAudioWithKeydown);
