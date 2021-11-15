bfac6821acaca0c4e41d01fca88b87ccad7e332a
tatiastep <tetiana.step.st@gmail.com>
2021-11-15 03:40:16 +0200

    function refactored

--- hw-11-promises/script.js
+++ hw-11-promises/script.js
@@ -2,19 +2,19 @@ const interval = 50; //ms
 
 async function getRandomChinese(length) {
   const time = length * interval;
-  let chineseWord = '';
+  let result = '';
 
-  for (let i = 0; i < length; i++) {
-    const symbol = new Promise((resolve) => {
+  while (length) {
+    const randomChinese = new Promise((resolve) => {
       setTimeout(() => {
         const sign = Date.now().toString().substr(-5);
         resolve(String.fromCharCode(sign));
       }, time)
     });
-    chineseWord += await symbol;;
+    result += await randomChinese;
+    length--;
   }
-  return chineseWord;
+  return result;
 };
 
 console.log(getRandomChinese(4));
-
