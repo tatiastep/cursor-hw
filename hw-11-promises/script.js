914291a131adf5f08ac49cd32b15ca132b8fd49c
tatiastep <tetiana.step.st@gmail.com>
2021-11-29 01:42:06 +0200

    refactoring

--- hw-11-promises/script.js
+++ hw-11-promises/script.js
@@ -3,18 +3,21 @@ const interval = 50; //ms
 async function getRandomChinese(length) {
   const time = length * interval;
   let result = '';
-
-  while (length) {
-    const randomChinese = new Promise((resolve) => {
-      setTimeout(() => {
-        const sign = Date.now().toString().substr(-5);
-        resolve(String.fromCharCode(sign));
-      }, time)
-    });
-    result += await randomChinese;
-    length--;
+  try {
+    while (length) {
+      const randomChinese = new Promise((resolve) => {
+        setTimeout(() => {
+          const sign = Date.now().toString().substr(-5);
+          resolve(String.fromCharCode(sign));
+        }, time)
+      });
+      result += await randomChinese;
+      length--;
+    }
+    return result;
+  } catch (err) {
+    console.log("Error: " + err.message);
   }
-  return result;
 };
 
-console.log(getRandomChinese(4));
+getRandomChinese(4).then((data) => console.log(data));
